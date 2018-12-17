5734c37b6dcc7642ecc8fce26428ed01ade8786f
seenevz <serneves@hotmail.com>
2018-12-17 10:59:00 +0000

    done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,25 @@
 class Instructor
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_student_test(student)
+    BoatingTest.all.select {|test| test.student == student}
+  end
+
+  def pass_student(student, test_name)
+      find_student_test(student) ?  (find_student_test(student).select {|test| test.test_status != "passed" })[0].test_status = "passed" : BoatingTest.new(student, test_name, "passed", self)
+  end
+
+  def fail_student(student, test_name)
+      find_student_test(student) ?  (find_student_test(student).select {|test| test.test_status != "failed" })[0].test_status = "failed" : BoatingTest.new(student, test_name, "failed", self)
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test( test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(student_name)
+    self.all.find {|student| student.name == student_name}
+  end
+
+  def student_grades
+    BoatingTest.all.find_all {|test| test.student.name == self.name}
+  end
+
+  def grade_percentage
+    (((student_grades.find_all {|test| test.test_status == "passed"}.length.to_f) / student_grades.length) * 100).round(2)
+  end
 
 end
